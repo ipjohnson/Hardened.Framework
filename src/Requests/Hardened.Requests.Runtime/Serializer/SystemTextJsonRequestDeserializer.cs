e16e2a720bfed1027ddcf757789359e197c80d76
Ian Johnson <ipjohnson123@yahoo.com>
2022-09-19 22:40:11 -0400

    adding logging output

--- src/Requests/Hardened.Requests.Runtime/Serializer/SystemTextJsonRequestDeserializer.cs
+++ src/Requests/Hardened.Requests.Runtime/Serializer/SystemTextJsonRequestDeserializer.cs
@@ -5,17 +5,20 @@ using Hardened.Requests.Abstract.Execution;
 using Hardened.Requests.Abstract.Serializer;
 using Hardened.Requests.Runtime.Configuration;
 using Hardened.Requests.Runtime.Errors;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
 
 namespace Hardened.Requests.Runtime.Serializer
 {
+
     public class SystemTextJsonRequestDeserializer : IRequestDeserializer
     {
         private readonly JsonSerializerOptions _serializerOptions;
-
-        public SystemTextJsonRequestDeserializer(IOptions<IJsonSerializerConfiguration> configuration)
+        private ILogger<SystemTextJsonRequestDeserializer> _logger;
+        public SystemTextJsonRequestDeserializer(IOptions<IJsonSerializerConfiguration> configuration, ILogger<SystemTextJsonRequestDeserializer> logger)
         {
+            _logger = logger;
             _serializerOptions = 
                 configuration.Value.DeSerializerOptions ?? 
                 new(JsonSerializerDefaults.Web);
@@ -34,7 +37,7 @@ namespace Hardened.Requests.Runtime.Serializer
             {
                 return await DeserializeEncodedContent<T>(context, contentEncoding);
             }
-
+            _logger.LogInformation($"Deserialize with option convert count {_serializerOptions.Converters.Count}");
             return await System.Text.Json.JsonSerializer.DeserializeAsync<T>(context.Request.Body, _serializerOptions);
         }
 
@@ -44,14 +47,14 @@ namespace Hardened.Requests.Runtime.Serializer
             {
                 await using var decompressStream = new GZipStream(context.Request.Body, CompressionMode.Decompress);
 
-                return await System.Text.Json.JsonSerializer.DeserializeAsync<T>(decompressStream);
+                return await System.Text.Json.JsonSerializer.DeserializeAsync<T>(decompressStream, _serializerOptions);
             }
             
             if (contentEncoding.Contains("br"))
             {
                 await using var decompressStream = new BrotliStream(context.Request.Body, CompressionMode.Decompress);
 
-                return await System.Text.Json.JsonSerializer.DeserializeAsync<T>(decompressStream);
+                return await System.Text.Json.JsonSerializer.DeserializeAsync<T>(decompressStream, _serializerOptions);
             }
             
             throw new BadContentEncodingException(contentEncoding);
