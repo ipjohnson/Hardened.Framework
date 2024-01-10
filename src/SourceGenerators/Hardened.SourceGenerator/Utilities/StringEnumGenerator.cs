2cf4b203e7ff6a61653e38b6f5e968103f4e0a09
Ian Johnson <ipjohnson123@yahoo.com>
2024-01-10 17:59:47 -0500

    upgrading to 8.0.0

---
+++ src/SourceGenerators/Hardened.SourceGenerator/Utilities/StringEnumGenerator.cs
@@ -0,0 +1,10 @@
+﻿using Hardened.SourceGenerator.Shared;
+using Microsoft.CodeAnalysis;
+
+namespace Hardened.SourceGenerator.Utilities;
+
+public class StringEnumGenerator {
+    public static void Setup(IncrementalGeneratorInitializationContext context, IncrementalValuesProvider<EntryPointSelector.Model> applicationModel) {
+        throw new NotImplementedException();
+    }
+}
